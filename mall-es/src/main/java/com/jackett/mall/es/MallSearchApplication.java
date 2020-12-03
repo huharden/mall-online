74759bc60054ff555de5598d5f608934f84482a8
hutao <15170073501@163.com>
2020-12-03 15:55:15 +0800

    Initial commit

---
+++ mall-es/src/main/java/com/jackett/mall/es/MallSearchApplication.java
@@ -0,0 +1,18 @@
+package com.jackett.mall.es;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @projectName：mall-online
+ * @className：Application
+ * @describe：TODO
+ * @createTime：2020/12/3 3:47 下午
+ * @author：HuTao
+ */
+@SpringBootApplication
+public class MallSearchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MallSearchApplication.class, args);
+    }
+}
